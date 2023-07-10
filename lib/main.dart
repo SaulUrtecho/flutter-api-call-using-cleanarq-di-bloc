e01dddd55fcc8c6ba44dba1a54ed7cb7e2905d6e
SaulUrtecho <saul.urtecho93@gmail.com>
2023-07-10 17:42:02 -0600

    adding bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:bloc_demo/di/di.dart';
+import 'package:bloc_demo/presentation/blocs/bloc_observer.dart';
 import 'package:bloc_demo/presentation/screens/main_screen/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = SimpleBlocObserver();
   await setupDependencies();
   runApp(const MyApp());
 }
@@ -14,7 +17,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter API Call',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
